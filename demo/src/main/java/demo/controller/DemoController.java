10b3bddb5696caca0dbc4858c51909367c0aee41
drtiti <lizitongdhu@126.com>
2019-02-12 21:33:17 +0800

    backend
    backend

--- demo/src/main/java/demo/controller/DemoController.java
+++ demo/src/main/java/demo/controller/DemoController.java
@@ -1,8 +1,15 @@
 package demo.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSONObject;
 
 @Controller
 public class DemoController {
@@ -14,5 +21,17 @@ public class DemoController {
 @ResponseBody
 public String test(){
 	return "this.is.a.test";
+}
+//用这种写法，前端URL为http://127.0.0.1:8080/demo/jsontest，demo是上下文根，jsontest对应value的值
+@RequestMapping(value="jsontest",method = {RequestMethod.POST})
+@ResponseBody
+public JSONObject jsontest(HttpServletRequest request){
+	Map m = request.getParameterMap();//获取前端参数
+	
+	JSONObject obj = new JSONObject();
+	obj.put("val1", "string1");
+	obj.put("val2", "string2");
+	obj.put("val3", "string3");
+	return obj;
 }
 }
